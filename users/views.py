fcb08dd42f4372efd2c59d7374769aff218c3031
egor <ega.nik5674@gmail.com>
2024-04-28 17:15:47 +0300

    Update users app

--- users/views.py
+++ users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from users.models import User
+from users.serializers import UsersSerializer
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    serializer_class = UsersSerializer
+
+
+class UserListAPIView(generics.ListAPIView):
+    serializer_class = UsersSerializer
+    queryset = User.objects.all()
+
+
+class UserRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = UsersSerializer
+    queryset = User.objects.all()
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = UsersSerializer
+    queryset = User.objects.all()
+
+
+class UserDestroyAPIView(generics.DestroyAPIView):
+    serializer_class = UsersSerializer
+    queryset = User.objects.all()
