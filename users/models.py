e3d5d0e2bed979b00df1a1f360e5059ab55f153b
egor <ega.nik5674@gmail.com>
2024-04-29 20:23:46 +0300

    Update users with new logic

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from service.models import NULLABLE
+from service.models import NULLABLE, Course, Lesson
 
 
 class User(AbstractUser):
@@ -24,3 +24,18 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'пользователь'
         verbose_name_plural = 'пользователи'
+
+
+class Payment(models.Model):
+    class PaymentMethod(models.TextChoices):
+        CASH = 'cash', 'Наличные'
+        CARD = 'card', 'Перевод на счёт'
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь', related_name='users')
+    date = models.DateTimeField(auto_now=True, verbose_name='Дата оплаты')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Оплаченный курс', **NULLABLE,
+                               related_name='courses')
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, verbose_name='Оплаченный урок', **NULLABLE,
+                               related_name='lessons')
+    amount = models.IntegerField(verbose_name='Сумма оплаты')
+    method = models.CharField(max_length=50, choices=PaymentMethod.choices, verbose_name='Способ оплаты')
--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import UserCreateAPIView, UserListAPIView, UserRetrieveAPIView, UserUpdateAPIView, UserDestroyAPIView
+from users.views import UserCreateAPIView, UserListAPIView, UserRetrieveAPIView, UserUpdateAPIView, UserDestroyAPIView, \
+    PaymentListAPIView, PaymentCreateAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
+    # User
     path('create/', UserCreateAPIView.as_view(), name='user_create'),
     path('', UserListAPIView.as_view(), name='user_list'),
     path('<int:pk>/', UserRetrieveAPIView.as_view(), name='user_retrieve'),
     path('edit/<int:pk>/', UserUpdateAPIView.as_view(), name='user_edit'),
-    path('delete/<int:pk>/', UserDestroyAPIView.as_view(), name='user_delete')
+    path('delete/<int:pk>/', UserDestroyAPIView.as_view(), name='user_delete'),
+
+    # Payment
+    path('payment/create/', PaymentCreateAPIView.as_view(), name='payment_create'),
+    path('payment/', PaymentListAPIView.as_view(), name='payment_list')
 ]
