7d13c725c55309f1999635f000dacaeb21ab554e
egor <ega.nik5674@gmail.com>
2024-05-03 13:16:46 +0300

    Update serializers

--- service/serializers.py
+++ service/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.permissions import IsAuthenticated
 
 from service.models import Course, Lesson
 
@@ -7,15 +8,17 @@ class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+        permission_classes = [IsAuthenticated]
 
 
 class CourseSerializer(serializers.ModelSerializer):
     lesson_count = serializers.SerializerMethodField()
-    lesson = LessonSerializer(many=True)
+    lessons = LessonSerializer(many=True, required=False)
 
     class Meta:
         model = Course
         fields = '__all__'
+        permission_classes = [IsAuthenticated]
 
     @staticmethod
     def get_lesson_count(instance):
