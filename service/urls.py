5e8e2c532095ead3e8dfd817d642f99eb241fb8d
egor <ega.nik5674@gmail.com>
2024-05-07 15:47:40 +0300

    Update service app

--- service/urls.py
+++ service/urls.py
@@ -4,7 +4,7 @@ from service.apps import ServiceConfig
 from rest_framework.routers import DefaultRouter
 
 from service.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
-    LessonUpdateAPIView, LessonDestroyAPIView
+    LessonUpdateAPIView, LessonDestroyAPIView, SubscriptionAPIView
 
 router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='courses')
@@ -12,9 +12,13 @@ router.register(r'courses', CourseViewSet, basename='courses')
 app_name = ServiceConfig.name
 
 urlpatterns = [
+    # Lesson
     path('lessons/create/', LessonCreateAPIView.as_view(), name='lesson_create'),
     path('lessons/', LessonListAPIView.as_view(), name='lesson_list'),
     path('lessons/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson_retrieve'),
     path('lessons/edit/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson_edit'),
-    path('lessons/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson_delete')
+    path('lessons/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson_delete'),
+
+    # Subscription
+    path('subscription/', SubscriptionAPIView.as_view(), name='subscription')
 ] + router.urls
